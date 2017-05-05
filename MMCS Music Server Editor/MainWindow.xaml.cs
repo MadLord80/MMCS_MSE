3003c3961b5cb7fd280c13e8c768419d96514b7c
MadLord <madlord@list.ru>
2017-05-05 17:24:36 +0700

    begin

--- MMCS Music Server Editor/MainWindow.xaml.cs
+++ MMCS Music Server Editor/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Forms;
+using System.IO;
 
 namespace MMCS_Music_Server_Editor
 {
@@ -20,9 +22,42 @@ namespace MMCS_Music_Server_Editor
     /// </summary>
     public partial class MainWindow : Window
     {
+        private FolderBrowserDialog opendir = new FolderBrowserDialog();
+
+        //paths
+        private string INFO_path = "\\INFO";
+        private string ALBUM_path, DISCID_path, HIST_path, RECORD_path, TITLE_path;
+
+        private string DATA_path = "\\DATA";
+        //private string CUSTOM_path = "\\CUSTOM";
+
+        //Settings
+        private string defALBUM_ID = "0000001";
+
         public MainWindow()
         {
             InitializeComponent();
+
+            ALBUM_path = INFO_path + "\\ALBUM";
+            DISCID_path = INFO_path + "\\DISCID";
+            HIST_path = INFO_path + "\\HIST";
+            RECORD_path = INFO_path + "\\RECORD";
+            TITLE_path = INFO_path + "\\TITLE";
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            if (opendir.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                string path = "D:\\Cloud\\домашнее\\outlander\\files\\sound\\music_server_from_n04_with_rus\\AVUNIT";
+                opendir.SelectedPath = path;
+
+                string dir_path = opendir.SelectedPath;
+                if (! Directory.Exists(dir_path + "\\INFO\\ALBUM\\ALBUM0000001") || ! File.Exists(dir_path + "\\INFO\\ALBUM\\ALBUM0000001\\ALBUM0000001.lst"))
+                {
+                    System.Windows.MessageBox.Show("No ALBUM0000001.lst!");
+                }
+            }
         }
     }
 }
--- MMCS Music Server Editor/Properties/AssemblyInfo.cs
+++ MMCS Music Server Editor/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("MMCS Music Server Editor")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCopyright("Copyright ©MadLord  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
