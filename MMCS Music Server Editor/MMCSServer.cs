6256882a8b858911b3358ed58e6df9b449acd7f7
MadLord <madlord@list.ru>
2017-05-10 17:03:45 +0700

    prepare to get groups

---
+++ MMCS Music Server Editor/MMCSServer.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace MMCS_MSE
+{
+    class MMCSServer
+    {
+        //paths
+        private string main_dir;
+        public string INFO_path = "\\INFO";
+        public string ALBUM_path, DISCID_path, HIST_path, RECORD_path, TITLE_path;
+
+        public string DATA_path = "\\DATA";
+        //private string CUSTOM_path = "\\CUSTOM";
+
+        //Settings
+        public string defALBUM_ID = "0000001";
+        //INDEX
+        public int cnt_disks_offset = 0x24;
+        public int cnt_groups = 100;
+        public int groups_offset = 0x28;
+        public int groups_length = 0x84;
+
+        public MMCSServer(string dir)
+        {
+            main_dir = dir;
+
+            ALBUM_path = INFO_path + "\\ALBUM";
+            DISCID_path = INFO_path + "\\DISCID";
+            HIST_path = INFO_path + "\\HIST";
+            RECORD_path = INFO_path + "\\RECORD";
+            TITLE_path = INFO_path + "\\TITLE";
+        }
+
+        public string get_ALBUMpath()
+        {
+            string path = main_dir + INFO_path + ALBUM_path + defALBUM_ID + "\\ALBUM" + defALBUM_ID + ".lst";
+            return path;
+        }
+
+        public string get_INDEXpath()
+        {
+            string path = main_dir + INFO_path + "\\INDEX.lst";
+            return path;
+        }
+    }
+}
--- MMCS Music Server Editor/MainWindow.xaml.cs
+++ MMCS Music Server Editor/MainWindow.xaml.cs
@@ -15,7 +15,7 @@ using System.Windows.Shapes;
 using System.Windows.Forms;
 using System.IO;
 
-namespace MMCS_Music_Server_Editor
+namespace MMCS_MSE
 {
     /// <summary>
     /// Логика взаимодействия для MainWindow.xaml
@@ -24,25 +24,12 @@ namespace MMCS_Music_Server_Editor
     {
         private FolderBrowserDialog opendir = new FolderBrowserDialog();
 
-        //paths
-        private string INFO_path = "\\INFO";
-        private string ALBUM_path, DISCID_path, HIST_path, RECORD_path, TITLE_path;
-
-        private string DATA_path = "\\DATA";
-        //private string CUSTOM_path = "\\CUSTOM";
-
-        //Settings
-        private string defALBUM_ID = "0000001";
+        private help_functions hf = new help_functions();
+        private MMCSServer mserver;
 
         public MainWindow()
         {
             InitializeComponent();
-
-            ALBUM_path = INFO_path + "\\ALBUM";
-            DISCID_path = INFO_path + "\\DISCID";
-            HIST_path = INFO_path + "\\HIST";
-            RECORD_path = INFO_path + "\\RECORD";
-            TITLE_path = INFO_path + "\\TITLE";
         }
 
         private void MenuItem_Click(object sender, RoutedEventArgs e)
@@ -53,9 +40,52 @@ namespace MMCS_Music_Server_Editor
                 opendir.SelectedPath = path;
 
                 string dir_path = opendir.SelectedPath;
-                if (! Directory.Exists(dir_path + "\\INFO\\ALBUM\\ALBUM0000001") || ! File.Exists(dir_path + "\\INFO\\ALBUM\\ALBUM0000001\\ALBUM0000001.lst"))
+                mserver = new MMCSServer(dir_path);
+
+                string info_path = mserver.get_INDEXpath();
+                if (! File.Exists(info_path))
                 {
-                    System.Windows.MessageBox.Show("No ALBUM0000001.lst!");
+                    System.Windows.MessageBox.Show(info_path + " not found!");
+                    return;
+                }
+                fill_groups_table(info_path);
+
+                //string album_path = mserver.get_ALBUMpath();
+                //if (! File.Exists(album_path))
+                //{
+                //    System.Windows.MessageBox.Show(album_path + " not found!");
+                //    return;
+                //}
+            }
+        }
+
+        private void fill_groups_table(string path)
+        {
+            FileStream fs = new FileStream(path, FileMode.Open, FileAccess.Read);
+            byte[] temp = new byte[0];
+
+            //fs.Position = mserver.cnt_disks_offset;
+            //int disks_count = fs.ReadByte();
+
+            ListBoxItem row = new ListBoxItem();
+
+            fs.Position = mserver.groups_offset;
+            byte[] group_desc = new byte[mserver.groups_length];
+            for (int i = 1; i <= mserver.cnt_groups; i++)
+            {
+                fs.Read(group_desc, 0, group_desc.Length);
+                hf.spliceByteArray(group_desc, ref temp, 0, 4);
+                if (BitConverter.ToUInt32(temp, 0) == 0x010000ff) break;
+
+                hf.spliceByteArray(group_desc, ref temp, 4, 4);
+                //←[tbl:NNN]
+                if (BitConverter.ToUInt32(temp,0) == 0x62745b1b)
+                {
+                    hf.spliceByteArray(group_desc, ref temp, 10, 3);
+                    if (hf.ByteArrayToString(temp) == "171")
+                    {
+                        //row.ad
+                    }
                 }
             }
         }
