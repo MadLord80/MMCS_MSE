8b2056636cf586ca8f65ae76527bd5bd39c93c52
MadLord <madlord@list.ru>
2017-05-12 17:16:18 +0700

    get group lists

--- MMCS Music Server Editor/MMCSServer.cs
+++ MMCS Music Server Editor/MMCSServer.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.IO;
+using System.ComponentModel;
 
 namespace MMCS_MSE
 {
@@ -10,14 +11,15 @@ namespace MMCS_MSE
     {
         //paths
         private string main_dir;
-        public string INFO_path = "\\INFO";
-        public string ALBUM_path, DISCID_path, HIST_path, RECORD_path, TITLE_path;
+		private string INFO_path = "\\INFO";
+		private string ALBUM_path = "\\ALBUM";
+		private string DISCID_path, HIST_path, RECORD_path, TITLE_path;
 
-        public string DATA_path = "\\DATA";
-        //private string CUSTOM_path = "\\CUSTOM";
+		private string DATA_path = "\\DATA";
+		//private string CUSTOM_path = "\\CUSTOM";
 
-        //Settings
-        public string defALBUM_ID = "0000001";
+		//Settings
+		private string defALBUM_ID = "0000001";
         //INDEX
         public int cnt_disks_offset = 0x24;
         public int cnt_groups = 100;
@@ -25,12 +27,25 @@ namespace MMCS_MSE
         public int group_length = 0x84;
         public int lists_offset;
         public int list_length = 8;
+		//ALBUM
+		public int max_lists = 100;
+		public int lists_size_offset = 0x24;
+		public int list_size_length = 4;
+		public int alists_offset;
+		//first 4 byte in list desc
+		public int a_unknown_length = 4;
+		public int listId_length = 4;
+		public int listsongs_length = 4;
+		public int listName_offset = 0x14;
+		public int listName_length = 0x180;
 
-        public MMCSServer(string dir)
+		public string MainDir
+		{
+			set { this.main_dir = value; }
+		}
+
+        public MMCSServer()
         {
-            main_dir = dir;
-
-            ALBUM_path = INFO_path + "\\ALBUM";
             DISCID_path = INFO_path + "\\DISCID";
             HIST_path = INFO_path + "\\HIST";
             RECORD_path = INFO_path + "\\RECORD";
@@ -38,11 +53,13 @@ namespace MMCS_MSE
 
             //INDEX
             lists_offset = groups_offset + cnt_groups * group_length;
-        }
+			//ALBUM
+			alists_offset = lists_offset + max_lists * list_size_length;
+		}
 
-        public string get_ALBUMpath()
+		public string get_ALBUMpath()
         {
-            string path = main_dir + INFO_path + ALBUM_path + defALBUM_ID + "\\ALBUM" + defALBUM_ID + ".lst";
+            string path = main_dir + INFO_path + ALBUM_path + ALBUM_path + defALBUM_ID + "\\ALBUM" + defALBUM_ID + ".lst";
             return path;
         }
 
@@ -62,4 +79,127 @@ namespace MMCS_MSE
             return data;
         }
     }
+
+    class MSGroup : INotifyPropertyChanged
+    {
+        private uint id;
+        private string name;
+		private uint[] lists;
+        
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public uint Id
+        {
+            get { return this.id; }
+            set
+            {
+                this.id = value;
+                OnPropertyChanged("Id");
+            }
+        }
+        public string Name
+        {
+            get { return this.name; }
+            set
+            {
+                //\x00 - end string
+                int null_offset = value.IndexOf('\x00');
+                this.name = (null_offset != -1) ? value.Substring(0, null_offset) : value;
+                OnPropertyChanged("Name");
+            }
+        }
+        public uint[] Lists
+        {
+            get { return this.lists; }
+            set
+            {
+                this.lists = value;
+                OnPropertyChanged("Lists");
+            }
+        }
+
+        public MSGroup(uint gid, string gname, uint[] glists)
+        {
+            this.id = gid;
+            //\x00 - end string
+            int null_offset = gname.IndexOf('\x00');
+            this.name = (null_offset != -1) ? gname.Substring(0, null_offset) : gname;
+            this.lists = glists;
+        }
+
+        protected void OnPropertyChanged(string name)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(name));
+            }
+        }
+    }
+
+	class MSList : INotifyPropertyChanged
+	{
+		private uint id;
+		private string name;
+		private uint songs_cnt;
+		private byte[,] songs;
+
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		public uint Id
+		{
+			get { return this.id; }
+			set
+			{
+				this.id = value;
+				OnPropertyChanged("Id");
+			}
+		}
+		public string Name
+		{
+			get { return this.name; }
+			set
+			{
+				//\x00 - end string
+				int null_offset = value.IndexOf('\x00');
+				this.name = (null_offset != -1) ? value.Substring(0, null_offset) : value;
+				OnPropertyChanged("Name");
+			}
+		}
+		public uint SongsCnt
+		{
+			get { return this.songs_cnt; }
+			set
+			{
+				this.songs_cnt = value;
+				OnPropertyChanged("SongsCnt");
+			}
+		}
+		public byte[,] Songs
+		{
+			get { return this.songs; }
+			set
+			{
+				this.songs = value;
+				OnPropertyChanged("Songs");
+			}
+		}
+
+		public MSList(uint lid, string lname, uint lscnt, byte[,] lsongs)
+		{
+			this.id = lid;
+			//\x00 - end string
+			int null_offset = lname.IndexOf('\x00');
+			this.name = (null_offset != -1) ? lname.Substring(0, null_offset) : lname;
+			this.songs_cnt = lscnt;
+			this.songs = lsongs;
+		}
+
+		protected void OnPropertyChanged(string name)
+		{
+			if (PropertyChanged != null)
+			{
+				PropertyChanged(this, new PropertyChangedEventArgs(name));
+			}
+		}
+	}
 }
--- MMCS Music Server Editor/MainWindow.xaml.cs
+++ MMCS Music Server Editor/MainWindow.xaml.cs
@@ -15,6 +15,7 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Windows.Forms;
 using System.IO;
+using System.Collections.ObjectModel;
 
 namespace MMCS_MSE
 {
@@ -25,13 +26,23 @@ namespace MMCS_MSE
     {
         private FolderBrowserDialog opendir = new FolderBrowserDialog();
 
-        private help_functions hf = new help_functions();
-        private MMCSServer mserver;
+		private string codePage = "iso-8859-5";
 
-        public MainWindow()
+        private help_functions hf = new help_functions();
+        private MMCSServer mserver = new MMCSServer();
+        private ObservableCollection<MSGroup> groups = new ObservableCollection<MSGroup>();
+		private ObservableCollection<MSList> lists = new ObservableCollection<MSList>();
+
+		public MainWindow()
         {
             InitializeComponent();
-        }
+            GroupsListView.ItemsSource = groups;
+			ListsListView.ItemsSource = lists;
+
+			radioButton1.IsChecked = true;
+
+			radioButton_Copy2.IsChecked = true;
+		}
 
         private void MenuItem_Click(object sender, RoutedEventArgs e)
         {
@@ -41,7 +52,7 @@ namespace MMCS_MSE
                 opendir.SelectedPath = path;
 
                 string dir_path = opendir.SelectedPath;
-                mserver = new MMCSServer(dir_path);
+				mserver.MainDir = dir_path;
 
                 string info_path = mserver.get_INDEXpath();
                 if (! File.Exists(info_path))
@@ -49,38 +60,35 @@ namespace MMCS_MSE
                     System.Windows.MessageBox.Show(info_path + " not found!");
                     return;
                 }
-                fill_groups_table(info_path);
-                
-
-                //string album_path = mserver.get_ALBUMpath();
-                //if (! File.Exists(album_path))
-                //{
-                //    System.Windows.MessageBox.Show(album_path + " not found!");
-                //    return;
-                //}
+                fill_groups_table(info_path);                
             }
         }
 
         private void fill_groups_table(string path)
         {
-            FileStream fs = new FileStream(path, FileMode.Open, FileAccess.Read);
+			lists.Clear(); groups.Clear();
+			FileStream fs = new FileStream(path, FileMode.Open, FileAccess.Read);
             byte[] temp = new byte[0];
 
             fs.Position = mserver.cnt_disks_offset;
-            int disks_count = fs.ReadByte();
+            int lists_count = fs.ReadByte();
             
             Hashtable groups_lists = new Hashtable();
-            for (int i = 0; i < disks_count; i++)
+            for (int i = 0; i < lists_count; i++)
             {
                 fs.Position = mserver.lists_offset + i * mserver.list_length + mserver.list_length - 1;
                 uint gid = (uint)fs.ReadByte();
-                if (groups_lists.ContainsKey(gid))
+
+				byte[] lid = new byte[mserver.listId_length];
+				fs.Position = mserver.lists_offset + i * mserver.list_length;
+				fs.Read(lid, 0, lid.Length);
+				if (groups_lists.ContainsKey(gid))
                 {
                     groups_lists[gid] = Convert.ToUInt32(groups_lists[gid]) + 1;
                 }
                 else
                 {
-                    groups_lists.Add(gid, 1);
+                    //groups_lists.Add(gid, new byte[1, 4] {lid});
                 }
             }
 
@@ -103,30 +111,175 @@ namespace MMCS_MSE
                 }
                 else
                 {
-                    group_name = hf.ByteArrayToString(hf.spliceByteArray(group_desc, ref temp, 4, group_desc.Length - 4), "iso-8859-5");
-                    //\x00 - end string
-                    int end_string_id = group_name.IndexOf('\x00');
-                    group_name = hf.ByteArrayToString(hf.spliceByteArray(group_desc, ref temp, 4, end_string_id), "iso-8859-5");
+                    group_name = hf.ByteArrayToString(hf.spliceByteArray(group_desc, ref temp, 4, group_desc.Length - 4), codePage);
                 }
 
                 uint lists_cnt = (groups_lists.ContainsKey(group_id)) ? Convert.ToUInt32(groups_lists[group_id]) : 0;
-                GroupsListView.Items.Add(new {id = group_id, name = group_name, lists = lists_cnt});
+                groups.Add(new MSGroup(group_id, group_name, lists_cnt));
             }
+			fs.Close();
         }
 
-        private void GroupsListView_onclick(object sender, MouseButtonEventArgs e)
+		private void fill_lists_table(uint gid)
+		{
+			lists.Clear();
+
+			string album_path = mserver.get_ALBUMpath();
+			if (!File.Exists(album_path))
+			{
+				System.Windows.MessageBox.Show(album_path + " not found!");
+				return;
+			}
+			FileStream fs = new FileStream(album_path, FileMode.Open, FileAccess.Read); ;
+			
+			byte[] temp = new byte[0];
+
+			uint[] lists_sizes = new uint[mserver.max_lists];
+
+			fs.Position = mserver.lists_size_offset;
+			for (int i = 0; i < lists_sizes.Length; i++)
+			{
+				byte[] list_size = new byte[mserver.list_size_length];
+				fs.Read(list_size, 0, list_size.Length);
+				uint size = BitConverter.ToUInt32(list_size, 0);
+				if (size == 0)
+				{
+					Array.Resize(ref lists_sizes, i);
+					break;
+				}
+				lists_sizes[i] = size;
+			}
+
+			fs.Position = mserver.alists_offset;
+			for (int i = 0; i < lists_sizes.Length; i++)
+			{
+
+			}
+
+			//Hashtable groups_lists = new Hashtable();
+			//for (int i = 0; i < disks_count; i++)
+			//{
+			//	fs.Position = mserver.lists_offset + i * mserver.list_length + mserver.list_length - 1;
+			//	uint gid = (uint)fs.ReadByte();
+			//	if (groups_lists.ContainsKey(gid))
+			//	{
+			//		groups_lists[gid] = Convert.ToUInt32(groups_lists[gid]) + 1;
+			//	}
+			//	else
+			//	{
+			//		groups_lists.Add(gid, 1);
+			//	}
+			//}
+
+			//fs.Position = mserver.groups_offset;
+			//byte[] group_desc = new byte[mserver.group_length];
+			//for (int i = 1; i <= mserver.cnt_groups; i++)
+			//{
+			//	fs.Read(group_desc, 0, group_desc.Length);
+			//	hf.spliceByteArray(group_desc, ref temp, 0, 4);
+			//	uint group_id = BitConverter.ToUInt32(temp, 0);
+			//	if (group_id == 0x010000ff) break;
+
+			//	string group_name = "";
+			//	hf.spliceByteArray(group_desc, ref temp, 4, 4);
+			//	//←[tbl:NNN]
+			//	if (BitConverter.ToUInt32(temp, 0) == 0x62745b1b)
+			//	{
+			//		string tbl_id = hf.ByteArrayToString(hf.spliceByteArray(group_desc, ref temp, 10, 3));
+			//		group_name = mserver.get_TBLdata(Convert.ToUInt32(tbl_id));
+			//	}
+			//	else
+			//	{
+			//		group_name = hf.ByteArrayToString(hf.spliceByteArray(group_desc, ref temp, 4, group_desc.Length - 4), "iso-8859-5");
+			//	}
+
+			//	uint lists_cnt = (groups_lists.ContainsKey(group_id)) ? Convert.ToUInt32(groups_lists[group_id]) : 0;
+			//	groups.Add(new MSGroup(group_id, group_name, lists_cnt));
+			//}
+			fs.Close();
+		}
+
+		private void GroupsListView_onclick(object sender, MouseButtonEventArgs e)
         {
             var listView = e.Source as System.Windows.Controls.ListView;
             if (listView != null)
             {
                 if (listView.SelectedItem != null)
                 {
-                    //object item = listView.Items.GetItemAt(listView.Items.IndexOf(listView.SelectedItem));
-                    object lbi = (listView.SelectedItem as {     });
-                    
-                    System.Windows.MessageBox.Show(lbi["name"]);                    
-                }
+                    MSGroup group = (listView.SelectedItem as MSGroup);
+					if (group.Id == 0)
+					{
+						//disks - ORG_ARRAY
+						//fill_disks_table();
+					}
+					else
+					{
+						//lists - ALBUM
+						fill_lists_table(group.Id);
+					}
+					//System.Windows.MessageBox.Show(group.Name);                    
+				}
             }
         }
-    }
+
+		private void radioButton_Checked(object sender, RoutedEventArgs e)
+		{
+			System.Windows.Controls.RadioButton li = (sender as System.Windows.Controls.RadioButton);
+			string dir = "";
+			switch (li.Content.ToString())
+			{
+				case "J-01":
+					dir = "D:\\Cloud\\домашнее\\outlander\\files\\sound\\music_server_from_j01\\AVUNIT";
+					break;
+				case "J-03":
+					dir = "D:\\Cloud\\домашнее\\outlander\\files\\sound\\music_server_from_j03\\AVUNIT";
+					break;
+				case "N-04":
+					dir = "D:\\Cloud\\домашнее\\outlander\\files\\sound\\music_server_from_n04\\AVUNIT";
+					break;
+				case "R-03 empty":
+					dir = "D:\\Cloud\\домашнее\\outlander\\files\\sound\\music_server_from_r03_empty\\AVUNIT";
+					break;
+				case "N-04rus":
+					dir = "D:\\Cloud\\домашнее\\outlander\\files\\sound\\music_server_from_n04_with_rus\\AVUNIT";
+					break;
+				case "N-04rus empty":
+					dir = "D:\\Cloud\\домашнее\\outlander\\files\\sound\\music_server_from_n04_with_rus\\AVUNIT_empty";
+					break;
+				case "N-04rus - 2s":
+					dir = "D:\\Cloud\\домашнее\\outlander\\files\\sound\\music_server_from_n04_with_rus\\AVUNIT_minus2songs";
+					break;
+				case "N-04rus newcd":
+					dir = "D:\\Cloud\\домашнее\\outlander\\files\\sound\\music_server_from_n04_with_rus\\AVUNIT_newcd";
+					break;
+				case "N-04rus necd - 1d":
+					dir = "D:\\Cloud\\домашнее\\outlander\\files\\sound\\music_server_from_n04_with_rus\\AVUNIT_newcd_minus1disc";
+					break;
+				case "N-04rus newcd only":
+					dir = "D:\\Cloud\\домашнее\\outlander\\files\\sound\\music_server_from_n04_with_rus\\AVUNIT_newcd_only";
+					break;
+				case "N-04rus newcd delother":
+					dir = "D:\\Cloud\\домашнее\\outlander\\files\\sound\\music_server_from_n04_with_rus\\AVUNIT_newcdonly_delother";
+					break;
+				default:
+					break;
+			}
+
+			mserver.MainDir = dir;
+			string info_path = mserver.get_INDEXpath();
+			if (!File.Exists(info_path))
+			{
+				lists.Clear(); groups.Clear();
+				System.Windows.MessageBox.Show(info_path + " not found!");				
+				return;
+			}
+			fill_groups_table(info_path);
+		}
+
+		private void radioButton1_Checked(object sender, RoutedEventArgs e)
+		{
+			System.Windows.Controls.RadioButton li = (sender as System.Windows.Controls.RadioButton);
+			codePage = li.Content.ToString();
+		}
+	}
 }
