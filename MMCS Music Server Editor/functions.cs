1a55f93441106b2c19555420251fe27cce1804ff
MadLord <madlord@list.ru>
2018-12-10 17:36:55 +0700

    save process

--- MMCS Music Server Editor/functions.cs
+++ MMCS Music Server Editor/functions.cs
@@ -8,6 +8,8 @@ namespace MMCS_MSE
 	{
 		public byte[] checksum32bit(byte[] data)
 		{
+			string cc = ToBase32String(data);
+
 			byte[] cs = new byte[4];
 			int hash = data.GetHashCode();
 			if (data.Length % 4 > 0) { return cs; }
@@ -20,6 +22,51 @@ namespace MMCS_MSE
 				cs[3] += data[i + 3];
 			}
 			return cs;
+		}
+
+		public static string ToBase32String(byte[] bytes)
+		{
+			string ValidChars = "QAZ2WSX3" + "EDC4RFV5" + "TGB6YHN7" + "UJM8K9LP";
+
+			StringBuilder sb = new StringBuilder();         // holds the base32 chars
+			byte index;
+			int hi = 5;
+			int currentByte = 0;
+
+			while (currentByte < bytes.Length)
+			{
+				// do we need to use the next byte?
+				if (hi > 8)
+				{
+					// get the last piece from the current byte, shift it to the right
+					// and increment the byte counter
+					index = (byte)(bytes[currentByte++] >> (hi - 5));
+					if (currentByte != bytes.Length)
+					{
+						// if we are not at the end, get the first piece from
+						// the next byte, clear it and shift it to the left
+						index = (byte)(((byte)(bytes[currentByte] << (16 - hi)) >> 3) | index);
+					}
+
+					hi -= 3;
+				}
+				else if (hi == 8)
+				{
+					index = (byte)(bytes[currentByte++] >> 3);
+					hi -= 3;
+				}
+				else
+				{
+
+					// simply get the stuff from the current byte
+					index = (byte)((byte)(bytes[currentByte] << (8 - hi)) >> 3);
+					hi += 5;
+				}
+
+				sb.Append(ValidChars[index]);
+			}
+
+			return sb.ToString();
 		}
 
 		public byte[] HexStringToByteArray(string hex)
